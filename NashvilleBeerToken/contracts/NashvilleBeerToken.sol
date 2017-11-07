a87d7c19e8e047b01ad3fbc3a798d836a4c081f8
dsummers91 <deonsummers91@gmail.com>
2017-11-06 23:33:44 -0600

    add date to logs

--- NashvilleBeerToken/contracts/NashvilleBeerToken.sol
+++ NashvilleBeerToken/contracts/NashvilleBeerToken.sol
@@ -6,9 +6,9 @@ contract NashvilleBeerToken {
   address public owner;
   mapping (address => uint256) balances;
 
-  event LogBeerClaimed(address indexed owner);
-  event LogBeerRedeemed(address indexed owner, bytes32 name);
-  event LogTransfer(address from, address indexed to);
+  event LogBeerClaimed(address indexed owner, uint256 date);
+  event LogBeerRedeemed(address indexed owner, bytes32 name, uint256 date);
+  event LogTransfer(address from, address indexed to, uint256 date);
 
   modifier onlyOwner {
     require(owner == msg.sender);
@@ -22,9 +22,9 @@ contract NashvilleBeerToken {
 
   function transfer(address _to, uint256 _amount) public returns(bool) {
     require(balances[msg.sender] - _amount <= balances[msg.sender]);
-    balances[msg.sender] -=  _amount;
+    balances[msg.sender] -= _amount;
     balances[_to] += _amount;
-    LogTransfer(msg.sender, _to);
+    LogTransfer(msg.sender, _to, now);
   }
 
   function balanceOf(address _owner) public constant returns(uint) {
@@ -34,7 +34,7 @@ contract NashvilleBeerToken {
   function redeemBeer(bytes32 _name) public returns(bool) {
     require(balances[msg.sender] > 0);
     balances[msg.sender]--;
-    LogBeerRedeemed(msg.sender, _name);
+    LogBeerRedeemed(msg.sender, _name, now);
   }
 
   function claimToken() public returns(bool) {
@@ -42,7 +42,7 @@ contract NashvilleBeerToken {
     require(totalSupply < maxSupply);
     balances[msg.sender]++;
     totalSupply++;
-    LogBeerClaimed(msg.sender);
+    LogBeerClaimed(msg.sender, now);
   }
 
   function assignToken(address _owner) public onlyOwner returns(bool) {
@@ -50,6 +50,6 @@ contract NashvilleBeerToken {
     require(totalSupply < maxSupply);
     balances[_owner]++;
     totalSupply++;
-    LogBeerClaimed(_owner);
+    LogBeerClaimed(_owner, now);
   }
 }
